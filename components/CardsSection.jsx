9699e10ed12ed9261716be24290103e8d2ad0f7b
anderson <anderson.d.cardoso92@gmail.com>
2022-07-14 11:32:22 -0500

    make cards responsive

--- components/CardsSection.jsx
+++ components/CardsSection.jsx
@@ -1,5 +1,5 @@
 import styles from "../styles/CardsSection.module.css";
-import Card from "./Card";
+import Image from "next/image";
 
 const CardsSection = () => {
   return (
@@ -8,21 +8,83 @@ const CardsSection = () => {
       <div className={styles.cardsWrapper}>
         <ul className={styles.section}>
           <li className={styles.sectionItem}>
-            <Card />
+            <>
+              <div className={styles.imageWrapper}>
+                <Image
+                  src="/img-9.jpeg"
+                  alt="card image"
+                  height={300}
+                  width={400}
+                />
+              </div>
+              <div className={styles.cardInfo}>
+                <h5>
+                  Explore the hidden waterfall deep inside the Amazon Jungle
+                </h5>
+              </div>
+            </>
           </li>
           <li className={styles.sectionItem}>
-            <Card />
+            <>
+              <div className={styles.imageWrapper}>
+                <Image
+                  src="/img-2.jpeg"
+                  alt="card image"
+                  height={300}
+                  width={400}
+                />
+              </div>
+              <div className={styles.cardInfo}>
+                <h5>Travel through the Islands of Bali in a Private Cruise</h5>
+              </div>
+            </>
           </li>
         </ul>
         <ul className={styles.section}>
           <li className={styles.sectionItem}>
-            <Card />
+            <>
+              <div className={styles.imageWrapper}>
+                <Image
+                  src="/img-3.jpeg"
+                  alt="card image"
+                  height={300}
+                  width={400}
+                />
+              </div>
+              <div className={styles.cardInfo}>
+                <h5>Experience Football on Top of the Himilayan Mountains</h5>
+              </div>
+            </>
           </li>
           <li className={styles.sectionItem}>
-            <Card />
+            <>
+              <div className={styles.imageWrapper}>
+                <Image
+                  src="/img-4.jpeg"
+                  alt="card image"
+                  height={300}
+                  width={400}
+                />
+              </div>
+              <div className={styles.cardInfo}>
+                <h5>Experience Football on Top of the Himilayan Mountains</h5>
+              </div>
+            </>
           </li>
           <li className={styles.sectionItem}>
-            <Card />
+            <>
+              <div className={styles.imageWrapper}>
+                <Image
+                  src="/img-8.jpeg"
+                  alt="card image"
+                  height={300}
+                  width={400}
+                />
+              </div>
+              <div className={styles.cardInfo}>
+                <h5>Ride through the Sahara Desert on a guided camel tour</h5>
+              </div>
+            </>
           </li>
         </ul>
       </div>
