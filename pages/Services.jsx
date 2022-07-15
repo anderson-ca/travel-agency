049a181743c3a13c13946f450c2d3ac4f0bc65f8
anderson <anderson.d.cardoso92@gmail.com>
2022-07-15 09:46:25 -0500

    finishe services page

--- pages/Services.jsx
+++ pages/Services.jsx
@@ -6,25 +6,31 @@ const Services = () => {
       <h1 className={styles.title}>Service Tiers</h1>
       <ul className={styles.serviceTable}>
         <li className={styles.tierCard}>
-          <h3>Basic</h3>
-          <p>✅ Tickes</p>
-          <p>❌ Hotel Booking</p>
-          <p>❌ 24h Service</p>
-          <p>❌ Restaurants</p>
+          <h3 className={styles.cardTitle}>Basic</h3>
+          <div className={styles.cardBody}>
+            <p>✅ Tickes</p>
+            <p>❌ Hotel Booking</p>
+            <p>❌ 24h Service</p>
+            <p>❌ Restaurants</p>
+          </div>
         </li>
         <li className={styles.tierCard}>
-          <h3>Basic</h3>
-          <p>✅ Tickes</p>
-          <p>✅ Hotel Booking</p>
-          <p>❌ 24h Service</p>
-          <p>❌ Restaurants</p>
+          <h3 className={styles.cardTitle}>Plus</h3>
+          <div className={styles.cardBody}>
+            <p>✅ Tickes</p>
+            <p>✅ Hotel Booking</p>
+            <p>❌ 24h Service</p>
+            <p>❌ Restaurants</p>
+          </div>
         </li>
         <li className={styles.tierCard}>
-          <h3>Basic</h3>
-          <p>✅ Tickes</p>
-          <p>✅ Hotel Booking</p>
-          <p>✅ 24h Service</p>
-          <p>✅ Restaurants</p>
+          <h3 className={styles.cardTitle}>Premium</h3>
+          <div className={styles.cardBody}>
+            <p>✅ Tickes</p>
+            <p>✅ Hotel Booking</p>
+            <p>✅ 24h Service</p>
+            <p>✅ Restaurants</p>
+          </div>
         </li>
       </ul>
     </div>
